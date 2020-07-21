37dadc2a36ca38a5348abe9c021ba02944192482
Soorya <sooriya.soni@gmail.com>
2020-07-21 19:58:42 +0100

    aws deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const connectDB = require('./config/db')
+const path = require('path')
+
 const app = express()
+
 
 // db connect
 connectDB()
@@ -8,7 +11,7 @@ connectDB()
 //INIT middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'))
+// app.get('/', (req, res) => res.send('API running'))
 
 //define routes
 
@@ -16,6 +19,14 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+//serve static in production 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
